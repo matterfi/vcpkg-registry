7cb963aa9e672697c5138d49440b481f856981bb
dhalman <d.halman@gmail.com>
2026-06-26 10:33:59 -0500

    Add libcput 0.2.0 port

---
+++ ports/libcput/portfile.cmake
@@ -0,0 +1,23 @@
+# Linux builds need system libatspi2.0-dev and pkg-config for libcput's AT-SPI probe.
+vcpkg_from_git(
+  OUT_SOURCE_PATH SOURCE_PATH
+  URL https://github.com/matterfi/libcput.git
+  REF 41fc747ee09effa234407cc175c0fb57ea99a52c
+  HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    -DBUILD_TESTING=OFF
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME libcput CONFIG_PATH lib/cmake/libcput)
+
+file(
+  REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
